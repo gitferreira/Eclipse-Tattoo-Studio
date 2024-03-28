16ecc9ea4d50fe1d891e313258ced9b54ad07585
gitferreira <iniferre96@gmail.com>
2024-03-28 20:32:20 +0100

    asd

--- src/components/SecondSection.js
+++ src/components/SecondSection.js
@@ -5,6 +5,7 @@ import {
   Monoton,
   Barlow_Condensed,
   Rubik_Mono_One,
+  Montserrat,
 } from "next/font/google";
 import Image from "next/image";
 import Card from "./Card";
@@ -12,33 +13,36 @@ import Card from "./Card";
 const monoton = Monoton({ subsets: ["latin"], weight: ["400"] });
 const barlow = Barlow_Condensed({ subsets: ["latin"], weight: ["300"] });
 const rubik = Rubik_Mono_One({ subsets: ["latin"], weight: ["400"] });
+const montse = Montserrat({ subsets: ["latin"], weight: ["400"] });
 
 export default function SecondSection() {
   return (
     <section className={styles.container}>
       <div className={styles.paddingContainer}>
-        <div className={`${styles.textContainer} ${rubik.className}`}>
-          <div className={styles.headingContainer}>
-            <p className={styles.subHeading}>our masters</p>
-            <div className={styles.heading}>
+        {/* <div className={`${styles.textContainer} ${rubik.className}`}> */}
+        {/* <div className={styles.headingContainer}> */}
+        {/* <p className={styles.subHeading}>our masters</p> */}
+        {/* <div className={styles.heading}>
               <h2 className={styles.line1}>Over 20 years </h2>
               <h2 className={styles.line2}>of experience</h2>
-            </div>
-          </div>
-        </div>
+            </div> */}
+        {/* </div> */}
+        {/* </div> */}
         <div className={styles.artistsContainer}>
           <div className={styles.mainContainer}>
             <div className={`${styles.mainText} ${monoton.className}`}>
               <h2 className={`${styles.title} ${rubik.className}`}>
                 Kate Adusk
               </h2>
-              <p className={`${styles.story} ${barlow.className}`}>
-                Meet <span className={styles.underline}>Kate Adusk</span>, the
-                visionary tattoo artist illuminating Eclipse Tattoo Studio with
-                her unparalleled talent. With a penchant for blending intricate
-                designs with celestial elements, Kate's work transcends
-                conventional artistry. For over a decade, her delicate yet
-                powerful creations have adorned the skin of clients worldwide.
+              <h4 className={`${styles.subTitle} ${rubik.className}`}>
+                the visionary tattoo artist illuminating Eclipse
+              </h4>
+              <p className={`${styles.story} ${montse.className}`}>
+                Meet <strong> Kate Adusk. </strong>
+                <strong>
+                  For over a decade, her delicate yet powerful creations have
+                  adorned the skin of clients worldwide.
+                </strong>
                 Kate's dedication to her craft and her ability to infuse each
                 piece with personal meaning have cemented her status as a true
                 luminary in the world of body art.
@@ -62,7 +66,9 @@ export default function SecondSection() {
             </div>
           </div>
 
-          <div className={styles.mainContainer}>
+          <div
+            className={`${styles.mainContainer} ${styles.invertedMainContainer}`}
+          >
             <div className={`${styles.photoContainer} ${styles.inverted} `}>
               <Card
                 cardImage={"/card2.png"}
@@ -76,13 +82,20 @@ export default function SecondSection() {
               <h2 className={`${styles.title2} ${rubik.className}`}>
                 Otis Winfre
               </h2>
-              <p className={`${styles.story} ${barlow.className}`}>
-                Meet <span className={styles.underline2}>Otis Winfre</span>, the
-                enigmatic artist behind Eclipse Tattoo Studio's most mesmerizing
-                designs. With a mysterious past shrouded in tales of wanderlust
-                and adventure, Jackson found solace and purpose within the
-                ink-stained walls of Eclipse. Each stroke of his needle tells a
-                story—of love, loss, and redemption.
+              <h4 className={`${styles.subTitle} ${styles.subTitle2} ${rubik.className}`}>
+
+              The Eclipse-Inspired Master;"Crafting Designs"
+              </h4>
+              <p className={`${styles.story} ${montse.className}`}>
+                Meet <strong>Otis Winfre </strong>, the enigmatic artist behind
+                Eclipse Tattoo Studio's most mesmerizing designs. 
+                Otis found solace and purpose within the ink-stained walls of
+                Eclipse.{" "}
+                <strong>
+                  {" "}
+                  Each stroke of his needle tells a story—of love, loss, and
+                  redemption.{" "}
+                </strong>
               </p>
               <Image
                 className={styles.signature}
--- src/components/ThirdSection.js
+++ src/components/ThirdSection.js
@@ -1,15 +1,24 @@
 import React from "react";
 import styles from "./ThirdSection.module.css";
 import Carousel from "./Carousel/Carousel";
+import { Montserrat, Rubik_Mono_One } from "next/font/google";
+
+const rubik = Rubik_Mono_One({ subsets: ["latin"], weight: ["400"] });
+const montse = Montserrat({ subsets: ["latin"], weight: ["400"] });
 
 export default function ThirdSection() {
   return (
     <section className={styles.container}>
-
-      <div className={styles.mainContainer}>    aa
-            </div>
-        <Carousel />
-    
+      <div className={styles.mainContainer}>
+      <div className={`${styles.textContainer} ${rubik.className}`}>
+        <p className={styles.subHeading}>Gallery</p>
+        <div className={styles.heading}>
+          <h2 className={styles.line1}>get some inspiration, </h2>
+          <h2 className={styles.line2}>Your next tattoo awaits</h2>
+        </div>
+        </div>
+      </div>
+      <Carousel />
     </section>
   );
 }
